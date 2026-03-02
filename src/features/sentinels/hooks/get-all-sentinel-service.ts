a2926f61ae913aa5868db019fb73966818841f5a
Alvaro Henrique <alvaroindaial15@gmail.com>
2026-03-02 12:43:25 -0300

    adicionado listagem de sentinelas

---
+++ src/features/sentinels/hooks/get-all-sentinel-service.ts
@@ -0,0 +1,34 @@
+import { database } from "@/lib/config"
+import { useQuery } from "@tanstack/react-query"
+import { collection, query, getDocs, limit } from "firebase/firestore"
+
+interface Sentinel {
+  id: string
+  active: boolean
+  name: string
+  code: string
+  category: string
+  stock: string
+  createdAt: Date
+}
+
+const getAllSentinelService = async (): Promise<Sentinel[]> => {
+  const q = query(collection(database, "sentinels"), limit(5))
+  const querySnapshot = await getDocs(q)
+
+  const results: Sentinel[] = []
+  querySnapshot.forEach((doc) => {
+    results.push({ id: doc.id, ...(doc.data() as Omit<Sentinel, "id">) })
+  })
+
+  return results
+}
+
+export const useGetAllSentinels = () => {
+  const { data: sentinels = [], isLoading, isError } = useQuery<Sentinel[]>({
+    queryKey: ['books'],
+    queryFn: getAllSentinelService,
+  })
+
+  return { sentinels, isLoading, isError }
+}
--- src/pages/sentinels.tsx
+++ src/pages/sentinels.tsx
@@ -1,12 +1,19 @@
 import { Navbar } from "@/components/shared/nav-bar";
 import { ModalRegisterSentinel } from "@/features/sentinels/components/modal-register-sentinel";
-import { Box, Button, Flex, Heading, Input, InputGroup } from "@chakra-ui/react";
+import { useGetAllSentinels } from "@/features/sentinels/hooks/get-all-sentinel-service";
+import { Box, Button, Flex, Heading, Input, InputGroup, Menu, Portal, Table } from "@chakra-ui/react";
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { LuSearch } from "react-icons/lu";
 
 export function Sentinels() {
   const [isOpen, setIsOpen] = useState(false)
+  const { sentinels, isLoading } = useGetAllSentinels()
+
+  const isSearching = false
+  const searchResults: any[] = []
+
+  const dataToRender = isSearching ? searchResults : sentinels
 
   return (
     <>
@@ -49,6 +56,86 @@ export function Sentinels() {
             <FiSearch />
           </Button>
         </Flex>
+
+         {/* Tabela responsiva */}
+                {!isLoading && (
+                  <Box overflowX="auto">
+                    <Table.Root size="sm" showColumnBorder>
+                      <Table.Header>
+                        <Table.Row>
+                          <Table.ColumnHeader>Nome</Table.ColumnHeader>
+                          <Table.ColumnHeader>Estoque</Table.ColumnHeader>
+                          <Table.ColumnHeader textAlign="center">Ações</Table.ColumnHeader>
+                        </Table.Row>
+                      </Table.Header>
+                      <Table.Body>
+                        {dataToRender.map((item: any) => (
+                          <Table.Row key={item.name}>
+                            <Table.Cell>{item.name}</Table.Cell>
+                            <Table.Cell>{item.stock}</Table.Cell>
+                            <Table.Cell textAlign="center">
+                              <Menu.Root>
+                                <Menu.Trigger asChild>
+                                  <Button variant="outline" size="sm">
+                                    Opções
+                                  </Button>
+                                </Menu.Trigger>
+                                <Portal>
+                                  <Menu.Positioner>
+                                    <Menu.Content>
+        
+                                      {/* <Menu.Item
+                                        onClick={() => {
+                                          setAddIsOpen(true)
+                                          setActualStock({
+                                            stock: parseInt(item.stock),
+                                            id: item.id
+                                          })
+                                        }}
+                                        value="enter">
+                                        Entrada Estoque
+                                      </Menu.Item> */}
+{/*         
+                                      <Menu.Item
+                                        value="out"
+                                        onClick={() => {
+                                          setOutIsOpen(true)
+                                          setActualStock({
+                                            stock: parseInt(item.stock),
+                                            id: item.id
+                                          })
+                                        }}
+                                      >
+                                        Saida Estoque
+                                      </Menu.Item> */}
+        
+                                      {/* <Menu.Item
+                                        value="edit"
+                                        onClick={() => {
+                                          setIsUpdate(true)
+                                          setValuesUpdate({ id: item.id, name: item.name })
+                                        }}>Editar</Menu.Item>
+         */}
+                                      {/* <Menu.Item
+                                        onClick={() => {
+                                          setIsDelete(true)
+                                          setPublicationDeleteId(item.id)
+                                        }}
+                                        value="delete">
+                                        Excluir
+                                      </Menu.Item> */}
+
+                                    </Menu.Content>
+                                  </Menu.Positioner>
+                                </Portal>
+                              </Menu.Root>
+                            </Table.Cell>
+                          </Table.Row>
+                        ))}
+                      </Table.Body>
+                    </Table.Root>
+                  </Box>
+                )}
 
       </Box>
     </>
